5031ceff3ea5309580463fcd32c757036a596413
luchiano9 <67933124+luchiano9@users.noreply.github.com>
2022-04-15 17:43:48 -0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Layout from "../components/layout";
+import Title from "../components/title";
+
+export default function Home() {
+  return (
+    <Layout>
+      <Title>Home Page</Title>
+
+      <p>Mi pagina de nextjs</p>
+      <style>
+        {`
+        p{color: darkgray;}
+        
+        p:hover{color: darkred;}
+        `}
+      </style>
+    </Layout>
+  );
+}
